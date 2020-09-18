0260dd0e36760e54d2f83df5f90eea75c944da4a
Nic Jackson <jackson.nic@gmail.com>
2020-09-18 07:45:06 +0100

    Update React Frontend to patch dependencies

---
+++ frontend/build/global.js
@@ -0,0 +1,4 @@
+window.global = {
+    api_location: 'http://localhost:9090',
+    files_location: 'http://localhost:9091'
+}
---
+++ frontend/build/precache-manifest.07457a1094222ea4a3bf206bbaa795ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b8abd8a7a74bd1f0203092f12cf6ed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a6a10dc44cd05bd3fb9",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2cfc4f71e7a9fc052c6b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a6a10dc44cd05bd3fb9",
+    "url": "/static/js/2.a16bd270.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a16bd270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfc4f71e7a9fc052c6b",
+    "url": "/static/js/main.35487cb3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
